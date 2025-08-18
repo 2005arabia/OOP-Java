e513329b4847dea998bb29ecaba7a91e625de03b
2005arabia <christianarabia134@gmail.com>
2025-08-18 14:43:10 +0800

    Add files via upload

---
+++ basicexercisepart1/Problem1.java
@@ -0,0 +1,20 @@
+package basicexercisepart1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Problem1 {
+    
+    public static void main(String[] args) {
+        System.out.println("name");
+        System.out.println("Christian Arabia");
+    }
+    
+    
+}
